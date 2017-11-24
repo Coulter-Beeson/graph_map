bed20cff1fd078f776317c91540c1f30c5b0686a
Coulter Beeson <cdbees@kokanee.cs.ubc.ca>
2017-11-23 23:48:03 -0800

    first commit of graph_map

---
+++ print_graph.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]){
+
+	if(argc != 3){
+		perror("Expects two arguments N and D");
+	}
+
+	int N = atoi(argv[1]);
+	int D = atoi(argv[2]);
+
+	//printf("%d",N);
+	//printf("%d",D);
+
+	int o,d;
+
+	//Print Offset Degree pairs
+	for(int i=0; i<N ; i++){
+		printf("%d",i);
+		printf("(");
+		o = getchar();
+		printf("%d", o);
+		printf(",");
+		d = getchar();
+		printf("%d",d);
+		printf(") ");
+	}
+
+	printf("\n");
+
+	int v;
+
+	//Print the Adjacency lists
+	for(int i=0; i<N*D ; i++){
+		if( i%D==0 ){
+			printf("%d [", i);
+		}
+
+		v = getchar();
+		printf("%d", v);
+
+		if( i%D==D-1 ){
+			printf("]");
+			printf("\n");
+			continue;
+		}
+
+		printf(",");
+
+	}
+
+	exit(EXIT_SUCCESS);
+
+}
