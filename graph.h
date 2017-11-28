6d57a25c4b1aa9ce8233c5a3a414c763eba568fd
Coulter Beeson <cdbees@kokanee.cs.ubc.ca>
2017-11-28 11:09:19 -0800

    graph compiles, untested

--- graph.h
+++ graph.h
@@ -1,6 +1,21 @@
 #ifndef GRAPH_H_   
 #define GRAPH_H_
 
-void add_edge(char* graph, int u, int v);  /* An example function declaration */
+#include <stdbool.h>
+
+struct graph{
+	char* g; //The underlying mmap'd file
+	int N;
+	int M;
+	int D;
+};
+
+struct graph* Graph(int fd);
+
+bool get_edge(struct graph* g, int u, int v);
+void add_edge(struct graph* g, int u, int v); 
+int get_off(struct graph* g, int u);
+int get_deg(struct graph* g, int u);
+void inc_deg(struct graph* g, int u);
 
 #endif // GRAPH_H_
