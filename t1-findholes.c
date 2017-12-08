6b72e31b58575c5764666c66dd30d1e9de995ea7
Laura <laura.cang@gmail.com>
2017-12-07 22:54:12 -0800

    edited findholes for easier display

--- t1-findholes.c
+++ t1-findholes.c
@@ -29,31 +29,42 @@ int main(int argc, char *argv[]) {
 	int N = atoi(argv[2]);
 	
 	char holes[N];
-	//holes[0]=-1;
-	printf("1 ");
+	
 	for (int i = 0; i < N; i++) {
 		holes[i]=0;
 	}
-	printf("2 ");
+
 	while(fscanf(fd, "%d %d", &u, &v)==2){
 		holes[u-1] = 1;
 		holes[v-1] = 1;
 	}
-	printf("3 ");
+	
 	FILE *fw;
-	fw = fopen(argv[3], "w"); 
+	fw = fopen(argv[3], "w+"); 
 	if (fw == NULL){
 		perror("error on opening file");
 		return(-1);
 	}
-	printf("4 ");
+
 	for (int i = 0; i<N; i++) {
 		if(holes[i]==0){
 			fprintf(fw, "%d %d\n", i+1, holes[i]);
-			printf("%d %d\n", i+1, holes[i]);
+			//printf("%d %d\n", i+1, holes[i]);
 		}
 	}
-	
+
+	rewind(fw);
+
+	int lines=0;
+
+	char ch;
+	while(!feof(fw)) 	{
+	  ch = fgetc(fw);
+	  if(ch == '\n') {
+		lines++;
+	  }
+	}
+	printf("H = %d\n", lines);
 	
 	fclose(fd);
 	fclose(fw);
