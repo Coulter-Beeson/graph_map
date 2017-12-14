d5d4e8f8142bf38c840cef85d8a865c2980f0a4f
DESKTOP-0J6U0HE\megha <meghanav@cs.ubc.ca>
2017-12-14 00:06:11 -0800

    added presentation

--- sample.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-declare -i COUNTER
-declare -a D=('2' '4' '8' '16' '32' '64' '128' '256' '512');
-COUNTER=0
-         while [  $COUNTER -lt 9 ]; do
-		echo ${D[$COUNTER]} 
-		let COUNTER=COUNTER+1 
-         done
